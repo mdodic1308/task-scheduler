60d31980159358a19df1032d800ad6574e888d85
Milan Dodic <milan.dodic@hpe.com>
2021-01-20 01:18:22 +0100

    Add update and delete functionality

--- scheduler-app/src/main/java/com/example/schedulerapp/data/SchedulerTaskRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.schedulerapp.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SchedulerTaskRepository extends JpaRepository<ScheduledTask, Long> {
-}
